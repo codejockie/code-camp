9b4db29dce4cf7d900382cd28dee3732bd823ed2
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-10-14 22:23:45 +0100

    get data from the camps controller

--- MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -1,19 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MyCodeCamp.Controllers
 {
-    public class CampsController : Controller
+  [Route("api/[controller]")]
+  public class CampsController : Controller
+  {
+    private ICampRepository _repo;
+
+    public CampsController(ICampRepository repo)
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+      _repo = repo;
     }
+
+    // GET: /<controller>/
+    [HttpGet("")]
+    public IActionResult Get()
+    {
+      var camps = _repo.GetAllCamps();
+      return Ok(camps);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id, bool includeSpeakers = false) {
+      try {
+        Camp camp = null;
+
+        if (includeSpeakers) {
+          camp = _repo.GetCampWithSpeakers(id);
+        } else {
+          camp = _repo.GetCamp(id); 
+        }
+
+        if (camp == null) return NotFound($"Camp {id} was not found.");
+
+        return Ok(camp);
+      } catch {
+        
+      }
+
+      return BadRequest();
+    }
+  }
 }
--- MyCodeCamp/Startup.cs
+++ MyCodeCamp/Startup.cs
@@ -8,37 +8,55 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MyCodeCamp.Data;
+using Newtonsoft.Json;
 
 namespace MyCodeCamp
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            _config = configuration;
-        }
-
-        IConfiguration _config;
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(_config);
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            loggerFactory.AddConsole(_config.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseMvc();
-        }
+      _config = configuration;
     }
+
+    IConfiguration _config;
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton(_config);
+      services.AddDbContext<CampContext>(ServiceLifetime.Scoped);
+      services.AddScoped<ICampRepository, CampRepository>();
+      services.AddTransient<CampDbInitializer>();
+
+      services.AddMvc()
+              .AddJsonOptions(options => {
+                options.SerializerSettings
+                       .ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app,
+                          IHostingEnvironment env,
+                          ILoggerFactory loggerFactory,
+                          CampDbInitializer seeder)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      loggerFactory.AddConsole(_config.GetSection("Logging"));
+      loggerFactory.AddDebug();
+
+      app.UseMvc(config =>
+      {
+        //config.MapRoute("default", "ap1/{controller}/{action}");
+      });
+
+      seeder.Seed().Wait();
+    }
+  }
 }
